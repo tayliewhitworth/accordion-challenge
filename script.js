f96d3338e262e3c1a7571045e485361791e35694
Taylie Whitworth <105079720+tayliewhitworth@users.noreply.github.com>
2022-07-28 15:56:54 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+console.log('Hey!')
+
+const accordion = document.getElementsByClassName('accordion-container')
+
+for (let i = 0; i < accordion.length; i++) {
+    accordion[i].addEventListener('click', function() {
+        this.classList.toggle('active')
+    })
+}
